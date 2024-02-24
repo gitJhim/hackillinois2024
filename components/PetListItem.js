6c4580085822415d63ad1de3b2773279ffd49cdd
Ben Russell <ben@benjamindrussell.com>
2024-02-24 18:21:01 -0500

    styled details view

--- components/PetListItem.js
+++ components/PetListItem.js
@@ -1,14 +1,21 @@
 // test
 import React, { useContext } from 'react';
-import { Text, Image, StyleSheet } from 'react-native';
+import { View, Text, Image, StyleSheet } from 'react-native';
 import {useAppContext} from '../context/AppContext';
 
 const nestImage = require("../assets/nest.png");
 
 const PetListItem = ({name, image}) => {
     return <>
-    <Image source={image} style={{width: 100, height: 100}}/>
-    <Text> Hello, my name is {name}, I am {Math.random()}</Text>
+    <View style={styles.petList}>
+    <Image source={image} style={styles.image}/>
+    <View style={styles.petListDetails}>    
+        <Text style={styles.text}>Name: {name}</Text>
+        <Text style={styles.text}>Birthday: 7/13/04</Text>
+        <Text style={styles.text}>Status: Happy</Text>
+    </View>
+
+    </View>
     </>;
 };
 
@@ -17,6 +24,22 @@ const styles = StyleSheet.create({
         width: 100,
         height: 100,
         resizeMode: 'contain',
+    },
+    petList: {
+        margin: 8,
+        padding: 8,
+        borderRadius: 4, 
+        borderWidth: 2,
+        borderColor: 'white',
+        flexDirection: 'row',
+        overflow: 'scroll',
+    },
+    petListDetails: {
+    },
+    text: {
+        color: 'white',
+        marginTop: 6,
+        marginBottom: 6,
     },
 });
 
