f7e8a7ab2ee44ddad83b6415dc483fc41c9d11e2
unknown <acharyanikhil2004@gmail.com>
2024-02-24 17:25:01 -0500

    updated pet movement

--- navigation/Screens/Garden.js
+++ navigation/Screens/Garden.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { View, Image, Dimensions, ImageBackground, StyleSheet } from 'react-native';
-import Animated, { useSharedValue, useAnimatedStyle, withSpring } from 'react-native-reanimated';
+import Animated, { useSharedValue, useAnimatedStyle, withSpring, withRepeat, withSequence  } from 'react-native-reanimated';
 import { useAppContext } from '../../context/AppContext';
 
 const { width, height } = Dimensions.get('window');
@@ -8,20 +8,51 @@ const backgroundImage = require("../../assets/garden-background.png");
 
 const Pet = ({ image }) => {
   const translateX = useSharedValue(0);
-  const translateY = useSharedValue(0);
+  const translateY = useSharedValue(height / 2);
+  const rotateZ = useSharedValue(0); // rotation value for wiggling
+
+  // Initial target setting
+  let targetX = Math.random() * (width - 100);
+  let targetY = Math.random() * ((height / 2 - 100) - 100) + height / 2;
 
   const movePetRandomly = () => {
-    translateX.value = withSpring(Math.random() * (width - 100));
-    translateY.value = withSpring(Math.random() * (height - 100));
+    // Set new target positions
+    targetX = Math.random() * (width - 100);
+    targetY = Math.random() * ((height / 2 - 100) - 100) + height / 2;
+
+    // Move to the new target
+    translateX.value = withSpring(targetX, {
+      duration: 10000,
+      damping: 20,
+      stiffness: 50,
+    });
+
+    translateY.value = withSpring(targetY, {
+      duration: 10000,
+      damping: 20,
+      stiffness: 50,
+    });
   };
 
   useEffect(() => {
-    const interval = setInterval(movePetRandomly, 2000);
-    return () => clearInterval(interval);
+    movePetRandomly();
+    // Start wiggle rotation
+    rotateZ.value = withRepeat(withSequence(
+      withSpring(-5, { damping: 20, stiffness: 50 }), // Rotate slightly left
+      withSpring(5, { damping: 20, stiffness: 50 })  // Rotate slightly right
+    ), -1, true); // Infinite repeat with autoreverse
+
+    // Change position less frequently to maintain a straight line movement
+    const positionInterval = setInterval(movePetRandomly, 5000);
+    return () => clearInterval(positionInterval);
   }, []);
 
   const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{ translateX: translateX.value }, { translateY: translateY.value }],
+    transform: [
+      { translateX: translateX.value },
+      { translateY: translateY.value },
+      { rotateZ: `${rotateZ.value}deg` },
+    ],
   }));
 
   return (
