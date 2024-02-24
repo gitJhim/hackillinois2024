7c02a8f05d63a4af760105b50d81f7d9e8b85385
Ben Russell <ben@benjamindrussell.com>
2024-02-24 17:20:15 -0500

    details screen now connected

--- navigation/Screens/DetailsScreen.js
+++ navigation/Screens/DetailsScreen.js
@@ -1,12 +1,12 @@
 import * as React from 'react';
+import {useAppContext} from '../../context/AppContext';
 import { View, Text } from 'react-native';
+import PetListItem from '../../components/PetListItem';
 
-export default function DetailsScreen({ navigation }) {
-    return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Details Screen</Text>
-        </View>
-    );
+export default function DetailsScreen({navigation}) {
+    const {state, dispatch} = useAppContext();
+
+    return<>
+    {state.pets.map(pet => (<PetListItem name={pet.name}/>))}
+    </>
 }
