948a49b028e0787874b4f5180c39260cac89cec7
TristanTran23 <tranquangthinhtdn@gmail.com>
2024-02-24 11:05:30 -0600

    Tristan Nav

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -2,13 +2,12 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import {View, Image} from 'react-native';
 
-// Screens
 import HomeScreen from './Screens/HomeScreen';
 import DetailsScreen from './Screens/DetailsScreen';
 import SettingScreen from './Screens/SettingScreen';
 
-//Screen names
 const homeName = "Home";
 const detailsName = "Details";
 const settingsName = "Settings";
@@ -16,16 +15,26 @@ const settingsName = "Settings";
 const Tab = createBottomTabNavigator();
 
 function MainContainer() {
+  const nTheme = {
+    dark: false,
+    colors: {
+        card: '#DABFFF',
+    }
+  }
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={nTheme} >
       <Tab.Navigator
         initialRouteName={homeName}
         screenOptions={({ route }) => ({
           tabBarIcon: ({ focused, color, size }) => {
             let iconName;
             let rn = route.name;
-
-            if (rn === homeName) {
+            
+            if (rn === "Egg") {
+                if (focused) {
+                    <Image source={require('../assets/egg-outline.png')}/>
+                }
+            } else if (rn === homeName) {
               iconName = focused ? 'home' : 'home-outline';
 
             } else if (rn === detailsName) {
@@ -37,6 +46,10 @@ function MainContainer() {
             return <Ionicons name={iconName} size={size} color={color} />;
           },
         })}
+        tabBarOptions={{
+            activeTintColot: "tomato",
+            inactiveTintColor: "grey",
+        }}
         >
 
         <Tab.Screen name={homeName} component={HomeScreen} />
