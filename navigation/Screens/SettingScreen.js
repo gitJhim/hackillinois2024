bade86c5a2bfa6a622063ce39e150c00d525e803
TristanTran23 <tranquangthinhtdn@gmail.com>
2024-02-25 02:19:18 -0600

    profile-screen

--- navigation/Screens/SettingScreen.js
+++ navigation/Screens/SettingScreen.js
@@ -1,14 +1,44 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet, Image } from 'react-native';
 
-export default function SettingScreen({navigation}) {
+
+function SettingScreen({navigation}) {
+    const avatarImg = require('../../assets/avatar.png');
     return(
-        <View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#4F518C' }}>
+            <Image source={avatarImg} style={styles.avatar}/>
             <Text
-            onPress={() => navigation.navigate('Home')}
+            style={{ fontSize: 20, color: 'white', fontWeight: 'bold', padding: 20, textAlign: 'center'}}
             >
-                User
+                gitJhim
             </Text>
+            <Pressable style={styles.button} onPress={() => navigation.navigate('Hatchery')}>
+                <Text style={styles.pressableText}>Start</Text>
+            </Pressable>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    button: {
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+        width: '33%' ,
+        height: '5%',
+        borderRadius: 4,
+    },
+    pressableText: {
+        color: '#4F518C',
+        fontSize: 22,
+        fontWeight: 'bold',
+    },
+    avatar: {
+        width: 150,
+        height: 150,
+        resizeMode: 'contain',
+        borderRadius: 75,
+    }
+});
+export default SettingScreen;
